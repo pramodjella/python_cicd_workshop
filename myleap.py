8fcf943902e6c0f55f0638b629310fd7d4cf4e58
pramod-jella <pramod.jella@isanghanminds.com>
2025-11-06 16:37:10 +0530

    first commit

---
+++ myleap.py
@@ -0,0 +1,3 @@
+def is_leap(yy):
+    """Functin to check given year is leap or not."""
+    return yy % 400 == 0  or (yy%4==0 and yy % 100 != 0)
---
+++ test_myleap.py
@@ -0,0 +1,17 @@
+from myleap import is_leap
+import pytest
+
+def test_leap():
+  assert is_leap(1992)==True
+
+def test_centruries_leap():
+  assert is_leap(2000)==True
+
+def test_centruries_nonleap():
+  assert is_leap(1900)==False
+
+def test_nonleap():
+  assert is_leap(1995)==False
+
+# https://www.lambdatest.com/blog/pytest-code-coverage-report/
+# https://coverage.readthedocs.io/en/7.6.1/
